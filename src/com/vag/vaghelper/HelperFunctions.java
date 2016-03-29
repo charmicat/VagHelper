87ca853ede8a58597add30540a7e9ba137ff5196
charmicat <charmicat@gmail.com>
2016-03-29 02:38:35 +0100

    first version

---
+++ src/com/vag/vaghelper/HelperFunctions.java
@@ -0,0 +1,52 @@
+package com.vag.vaghelper;
+
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.util.Log;
+
+public class HelperFunctions {
+
+	private static String TAG = "HelperFunctions";
+
+	// Determine if servicename is currently running
+	public static boolean isServiceRunning(Context context, String servicename) {
+		ActivityManager manager = (ActivityManager) context
+				.getSystemService(Context.ACTIVITY_SERVICE);
+		for (RunningServiceInfo service : manager
+				.getRunningServices(Integer.MAX_VALUE)) {
+			Log.d(TAG, service.service.getClassName());
+			if (servicename.equals(service.service.getClassName())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Indicates whether the specified action can be used as an intent. This
+	 * method queries the package manager for installed packages that can
+	 * respond to an intent with the specified action. If no suitable package is
+	 * found, this method returns false.
+	 * 
+	 * @param context
+	 *            The application's environment.
+	 * @param action
+	 *            The Intent action to check for availability.
+	 * 
+	 * @return True if an Intent with the specified action can be sent and
+	 *         responded to, false otherwise.
+	 */
+	public static boolean isIntentAvailable(Context context, String action) {
+		final PackageManager packageManager = context.getPackageManager();
+		final Intent intent = new Intent(action);
+		List<ResolveInfo> list = packageManager.queryIntentActivities(intent,
+				PackageManager.MATCH_DEFAULT_ONLY);
+		return list.size() > 0;
+	}
+}
